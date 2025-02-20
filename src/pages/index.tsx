54b1eb63f5ecfbf9c31dd88a37405f78b84ece64
Hans Ko <iamhansko@kakao.com>
2025-02-21 03:09:27 +0900

    ✏️ Fix: Landing Page with Iframe Tag

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Home(): JSX.Element {
+  return (
+    <iframe className={'mainPage'} src="https://main.d4c1ssihowx.amplifyapp.com/" />
+  );
+}
